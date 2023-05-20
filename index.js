a0e36cbe2141593a5f0b4ddde5d36651e6273882
Sebastian Galaz <seb.galaz@duocuc.cl>
2023-05-20 01:51:00 -0400

    metodo registros persona

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
-const port = 3000; // Puedes usar cualquier otro puerto de tu elección
+const port = 4000; // Puedes usar cualquier otro puerto de tu elección
 //idp2023*
 const connection = mysql.createConnection({
     host: 'localhost', // Cambia esto si tu base de datos está en otro servidor
     user: 'integracion',
-    password: 'idp2023',
+    password: 'idp2023*',
     database: 'integracion'
+  });
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
   });
   
 connection.connect((error) => {
@@ -20,13 +26,18 @@ connection.connect((error) => {
 
 //get  
 
-app.get('/registros', (req, res) => {
-    connection.query('SELECT * FROM tabla', (error, results) => {
-      if (error) {
-        console.error('Error al obtener los registros: ', error);
-        res.status(500).send('Error en el servidor');
-      } else {
-        res.json(results);
-      }
-    });
+app.get('/registros/:rut', (req, res) => {
+  const rut = req.params.rut;
+  connection.query('SELECT * FROM persona WHERE rut = ?', [rut], (error, results) => {
+    if (error) {
+      console.error('Error al obtener los registros: ', error);
+      res.status(500).send('Error en el servidor');
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor API escuchando en el puerto ${port}`);
 });
